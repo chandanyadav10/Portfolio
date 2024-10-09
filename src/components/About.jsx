f3b891fcb9bd08275479e5881d93302017754cc6
chandanyadav10 <chan190202@gmail.com>
2024-10-10 00:21:51 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Chandan, a passionate Web developer with a keen eye for MERN
+          Stack . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education
+        </h1>
+        <span>
+          [B.Tech(Information Technology)], [Oriental college of technology], [2020-2024] 
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in [Java, JavaScript, React, NodeJs, MongoDB, OOPs, Data Structures and Algorithms, SQL , ExpressJs, tailwind ] Experienced with [VS code, Git & Github
+          ] 
+          Excellent problem-solving skills Effective communicator and
+          collaborator
+        </span>
+        <br />
+        <br />
+        
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements
+        </h1>
+        <span>
+          [5 star on Hacker Rank, Newbie on code forces, Solve 200+ Coding questions on different platforms.]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative IT solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
